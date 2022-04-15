e0b6bb81244221d3adcf1ae36fdf80a0e8bc1d01
xiaozhuo <www.85295789@qq.com>
2022-04-15 17:53:00 +0800

    部署项目

--- BBS/settings.py
+++ BBS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@ua!gosi&ejr394*as7@g24v)6gpeepy=uvf00mny(+1yczp8h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,7 +125,7 @@ USE_TZ =False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# 静态文件收集目录
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
