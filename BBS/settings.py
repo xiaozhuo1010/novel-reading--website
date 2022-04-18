a2a5788dcde3034b1aafe0f454899258b54a5229
xiaozhuo <www.85295789@qq.com>
2022-04-18 21:12:26 +0800

    debug

--- BBS/settings.py
+++ BBS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@ua!gosi&ejr394*as7@g24v)6gpeepy=uvf00mny(+1yczp8h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
